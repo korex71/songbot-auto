3fe3eb0966d249b2e454e64f24e29bde3ba312c0
Gabriel <kore.dv7@gmail.com>
2025-04-06 04:08:48 -0300

    fix build linux

--- build.rs
+++ build.rs
@@ -1,5 +1,12 @@
+use std::path::Path;
+
 fn main() { 
-    let mut res = winres::WindowsResource::new(); 
-    res.set_icon("icon.ico"); // caminho do seu ícone 
-    res.compile().unwrap(); 
+    let icon_path = "icon.ico";
+    if Path::new(icon_path).exists() {
+        let mut res = winres::WindowsResource::new();
+        res.set_icon(icon_path);
+        res.compile().unwrap();
+    } else {
+        println!("cargo:warning=Icon file not found, skipping resource embedding");
+    }
 }
