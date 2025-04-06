d9830f6691a6b03d52527f12fc4b7e91a40d4af1
Gabriel <kore.dv7@gmail.com>
2025-04-06 04:13:31 -0300

    build windows

--- build.rs
+++ build.rs
@@ -1,6 +1,11 @@
 use std::path::Path;
 
 fn main() { 
+    if !cfg!(target_os = "windows") {
+        println!("cargo:warning=Ignoring build.rs.");
+        return;
+    }
+
     let icon_path = "icon.ico";
     if Path::new(icon_path).exists() {
         let mut res = winres::WindowsResource::new();
